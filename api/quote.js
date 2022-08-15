1fae1cc7b14306af9096c1ea0ae28c7641f6c1bb
Craig England <craigengland21@gmail.com>
2022-08-15 15:00:52 +0100

    feat: add serverless function

---
+++ api/quote.js
@@ -0,0 +1,31 @@
+const quotes = require("../quotes");
+const template = require("../template");
+const { MailtrapClient } = require("mailtrap");
+
+require("dotenv").config();
+
+export default function quote() {
+  const client = new MailtrapClient({
+    endpoint: process.env.MAILTRAP_API_ENDPOINT,
+    token: process.env.MAILTRAP_API_TOKEN,
+  });
+  const sender = {
+    email: process.env.FROM_EMAIL,
+    name: "Your quotation",
+  };
+  const recipients = [
+    {
+      email: process.env.RECIPIENT_EMAIL,
+    },
+  ];
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  client
+    .send({
+      from: sender,
+      to: recipients,
+      subject: "Your quote from Winnie the Pooh and friends!",
+      html: template(quote),
+    })
+    .then(console.log, console.error);
+}
